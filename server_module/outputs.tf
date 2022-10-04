653dcc4e7e9305745bf53aafc46e866449458aa4
VlN9 <tron4ik.ne@gmail.com>
2022-10-04 16:43:16 +0300

    add outputs

--- server_module/outputs.tf
+++ server_module/outputs.tf
@@ -5,3 +5,11 @@ output "aws_instance_public_ip" {
 output "aws_instance_id" {
   value = aws_instance.web_server[count.index]
 }
+
+output "sg_id" {
+  value = aws_security_group.wagtail_web_sg.id
+}
+
+output "instance_id" {
+  value = aws_instance.web_server.id
+}
