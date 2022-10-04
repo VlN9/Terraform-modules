eab4666e5b6edced197bb29fecc58454c7d6e724
VlN9 <tron4ik.ne@gmail.com>
2022-10-04 19:19:34 +0300

    ok

--- server_module/outputs.tf
+++ server_module/outputs.tf
@@ -1,9 +1,9 @@
 output "aws_instance_public_ip" {
-  value = aws_instance.web_server[*]
+  value = aws_instance.web_server[*].public_ip
 }
 
 output "aws_instance_id" {
-  value = aws_instance.web_server[*]
+  value = aws_instance.web_server[*].id
 }
 
 output "sg_id" {
