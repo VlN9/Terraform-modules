0d9578c4ad98782d6d4d138dd0d48f025872799c
VlN9 <tron4ik.ne@gmail.com>
2022-11-25 12:51:59 +0300

    remove default variables for user data

--- server_module/variables.tf
+++ server_module/variables.tf
@@ -28,19 +28,7 @@ variable "owner" {
   default = "Vladimir Nechay"
 }
 
-variable "user_data" {
-  default =<<EOF
-#!/bin/bash
-sudo yum -y upgrade
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo yum -y install git
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose 
-EOF 
-}
+variable "user_data" {}
 
 variable "security_groups" {
   type = list
