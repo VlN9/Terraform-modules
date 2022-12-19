7a34caab21b451078d497f8a815eb3fbac0d8f9e
VlN9 <tron4ik.ne@gmail.com>
2022-12-19 20:51:19 +0200

    correctness outputs

--- server_module/outputs.tf
+++ server_module/outputs.tf
@@ -5,3 +5,6 @@ output "aws_instance_public_ip" {
 output "aws_instance_id" {
   value = aws_instance.web_server[*].id
 }
+output "aws_instanse_prvt_ip" {
+  value = aws_instance.web_server[*].private_ip
+}
